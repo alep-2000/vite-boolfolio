b02f38d46b8a82284c4f18784f4f89135e90666d
alep-2000 <alessiopiras056@gmail.com>
2023-08-29 15:41:11 +0200

    Create index to posts

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,76 @@
 <script>
-export default {
+import axios from 'axios';
+import AppLoader from './AppLoader.vue';
 
+
+export default {
+    name: 'AppMain',
+    components: {
+        AppLoader
+    },
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000',
+            posts: [],
+            loading: true,
+            maxNumChara: 150
+        }
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/posts`).then((response) => {
+                if (response.data.success) {
+                    this.posts = response.data.results;
+                    this.loading = false;
+                }
+                else {
+
+                }
+            })
+        },
+        truncateText(text) {
+            if (text.length > this.maxNumChara) {
+                return text.substr(0, this.maxNumChara) + '...';
+            }
+
+            return text;
+        }
+    }
 }
 </script>
 
 <template lang="">
-    <div>
-        Ciao
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center text-secondary">Boolpress</h1>
+            </div>
+        </div>
+    </div>
+    <AppLoader v-if="loading" />
+    <div v-else class="container">
+        <div class="row">
+            <div class="col-12 col-md-4" v-for="post in posts" :key="post.id">
+                <div class="card-header my-3">
+                    {{ post.title }}
+                </div>
+                <div class="card-image-top my-3">
+                    <img :src="`${baseUrl}/storage/${post.cover_image}`" class="w-50">
+                </div>
+                <div class="card-body my-3">
+                    {{ truncateText(post.content) }}
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+img {
+    min-height: 200px;
+}
 </style>
