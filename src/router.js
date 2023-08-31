2117a28597f1dca1f54bbadbe5c7ba5f044e1cb6
alep-2000 <alessiopiras056@gmail.com>
2023-08-31 15:59:12 +0200

    Add page error 404 (Exercise Finisched)

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from './pages/HomePage.vue';
 import PostList from './pages/PostList.vue';
 import SinglePost from './pages/SinglePost.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -21,6 +22,15 @@ const router = createRouter({
             path: '/posts/:slug',
             name: 'single-post',
             component: SinglePost
+        },
+        {
+            path: '/pagina-non-trovata',
+            name: 'not-found',
+            component: NotFound
+        },
+        {
+            path: '/:catchAll(.*)',
+            redirect: '/pagina-non-trovata'
         }
     ]
 });
